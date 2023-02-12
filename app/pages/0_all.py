8d8af583b6de0fb75f486fb285167dcf51027733
mbkranz <kranz-michael@norc.org>
2023-02-12 10:35:51 -0600

    created combined excel in core_measures package; created before app deployment; added viewer to combined excel in long format

--- app/pages/0_all.py
+++ app/pages/0_all.py
@@ -1,41 +1,18 @@
 import streamlit as st 
-from core_measures.app import REPO_DIR
+import requests
+from core_measures.app.utils import REPO_DIR,make_agrid
+from pathlib import Path
+import time
 
-def load_csvs():
-    schema_list = ['staff-baseline','baseline','time-points','staff-time-points']
-    url_temp = REPO_DIR+"csvs/table-schema-{schema_name}.csv"
-    return {
-        schema_name:pd.read_csv(url_temp.format(schema=schema_name)) 
-        for schema_name in schema_list
-    }
-
-
-def download_excel():
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer,engine="xlsxwriter") as writer:
-        for name,item in load_csvs().items():
-            try:
-                df = pd.DataFrame(item)
-            except ValueError:
-                df = pd.DataFrame([item])
-
-            df.to_excel(writer,sheet_name=name)
-
-            # Get the workbook and the first worksheet
-            worksheet = writer.sheets[name]
-
-            # Freeze the headers (first row)
-            worksheet.freeze_panes = 'A2'
-
-            # Wrap header text
-            for row in worksheet.iter_rows(min_row=1, max_col=worksheet.max_column, max_row=1):
-                for cell in row:
-                    cell.alignment = cell.alignment.copy(wrap_text=True)
-        
-        writer.save()
-    
-    return buffer
+url = f"{REPO_DIR}/xlsx/core_measures.xlsx"
+url_long = f"{REPO_DIR}/xlsx/core_measures_long.xlsx"
+excel = requests.get(url).content
+excel_long = requests.get(url_long).content
+current_date = time.strftime("%Y_%m_%d")
 
 st.download_button(f"Click here to download all core measure data dictionaries",
-    data=download_excel(),
-    file_name="core-measure-fields.xlsx")
+    data=excel,
+    file_name=Path(url).name+"_"+current_date)
+
+df = pd.read_csv(excel_long)
+make_agrid(df)
