37fe589d2f70fa95ecf7332a0d157bc2325e840f
mbkranz <kranz-michael@norc.org>
2023-02-12 11:02:38 -0600

    added openpyxl ext

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
         "streamlit-aggrid",
         "streamlit",
         "frictionless==4.40.8",
-        "xlsxwriter"
+        "xlsxwriter",
+        "openpyxl"
     ],
     extras_require={
         'healdata_utils': ["healdata_utils @ git+https://github.com/norc-heal/healdata-utils.git"]
