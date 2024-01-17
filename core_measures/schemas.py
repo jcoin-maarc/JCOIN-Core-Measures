515eed6771ed0f4eb825cbc978a47751c0b9fb30
Michael Kranz <mbkranz@gmail.com>
2024-01-17 16:02:41 -0600

    del and simplify fxns give new  build workflow

--- core_measures/schemas.py
+++ core_measures/schemas.py
@@ -9,11 +9,11 @@ from pathlib import Path
 import click
 import re
 import petl as etl
-from core_measures.utils import combine_schemas_to_excel,json_to_df,CsvToJson
+from core_measures.utils import combine_schemas_to_excel,json_to_df
 from frictionless import Schema
 
-jsons = list(Path(__file__).parents[1].joinpath("schemas").glob("*"))
-csvs = list(Path(__file__).parents[1].joinpath("csvs").glob("*"))
+jsons = list(Path(__file__).parents[1].joinpath("schemas").glob("*.json"))
+csvs = list(Path(__file__).parents[1].joinpath("csvs").glob("*.csv"))
 
 def to_csv():
    
@@ -22,46 +22,10 @@ def to_csv():
         outdir = path.parent.with_stem("csvs")
         df.to_csv(outdir / path.with_suffix(".csv").name,index=False)
 
-    # create excel
-    xlsxpath = Path(outdir.with_name("xlsx"))
+
+
+def to_excel():
+    xlsxpath = Path(csvs[0].parent.with_name("xlsx"))
     xlsxpath.mkdir(exist_ok=True)
     combine_schemas_to_excel(csvs,str(xlsxpath/"core_measures.xlsx"))
     combine_schemas_to_excel(csvs,str(xlsxpath/"core_measures_long.xlsx"),separate_sheets=False)
-
-def update_json():
-
-    # convert csv to json
-    for path in csvs:
-        csvpath = path
-        schemapath = path.parent.with_stem("schemas") / path.with_suffix(".json").name
-        assert schemapath.exists(), f"Please make sure {schemapath.name} exists."
-        CsvToJson(csvpath).to_json(schemapath)
-    
-    # run tocsv to make sure all files encoded properly and xlsx file updated
-    to_csv()
-
-# click commands
-@click.group()
-def cli():
-    pass
-
-tocsv_help = """
-Flattens the json spec to a csv using the healdata_utils tool
-"""
-
-@click.command(name="tocsv",help=tocsv_help)
-def clito_csv():
-    to_csv()
-
-update_json_help = """
-update a json schema with fields and properties from csv file
-"""
-@click.command(name="updatejson",help=update_json_help)
-def cliupdate_json():
-    update_json()
-
-cli.add_command(cliupdate_json)
-cli.add_command(clito_csv)
-
-if __name__ == "__main__":
-    cli()
