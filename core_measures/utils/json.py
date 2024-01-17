f6464fa8b3d255fc720e6cc82d03f661f2132d9b
Michael Kranz <mbkranz@gmail.com>
2024-01-17 11:05:03 -0600

    module --> section (align with heal standards)

--- core_measures/utils/json.py
+++ core_measures/utils/json.py
@@ -7,7 +7,7 @@ def json_to_df(path):
     # convert json to csv
     table_fields = pd.json_normalize(json.loads(Path(path).read_text())['fields']).convert_dtypes()
     headers = table_fields.columns.to_list()
-    cols = [c for c in ['module','name','title','type','description','trueValues','falseValues',
+    cols = [c for c in ['section','name','title','type','description','trueValues','falseValues',
         'constraints.enum'] if c in headers]
     cols.extend([c for c in headers if not c in cols])
     #format
