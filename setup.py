c9c24204054a8cb4b15072dbe382eb1ab7c94da0
Michael Kranz <mbkranz@gmail.com>
2024-03-12 12:15:10 -0500

    install fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_namespace_packages
 setup(
     name='core_measures',
     install_requires=[
-        "https://github.com/norc-heal/healdata-utils.git@848939fcf8de41d0462efa922fa30c4273a84a94",
+        "healdata-utils @ git+https://github.com/norc-heal/healdata-utils.git@848939fcf8de41d0462efa922fa30c4273a84a94",
         "streamlit",
         "frictionless==4.40.8",
         "xlsxwriter",
